cbb74d9b04613fafd3119616cefb77a32177dec9
Santhosh Kumar Devaraj <39290866+Santhoshkumardevaraj@users.noreply.github.com>
2021-06-06 21:34:18 +0530

    Create count string in text.py

---
+++ count string in text.py
@@ -0,0 +1,12 @@
+print('\nReturn the count of sub-string “Emma” appears in the given string')
+
+str_var = "Emma is good developer. Emma is a writer"
+
+print(str_var.count('Emma'))
+
+#problem#
+
+print('\nPrint the triangle pattern')
+
+for i in range(1,6):
+    print(str(i)*i)
